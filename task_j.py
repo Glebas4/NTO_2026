f5256c0407506e610fd16653a88c4fed6557149e
Glebas4 <gleb.gurov.jr@gmail.com>
2025-11-12 19:39:39 +0300

    Update task_j.py

--- task_j.py
+++ task_j.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.spatial.transform import Rotation as R
 
 
 def get_data():
@@ -8,12 +7,20 @@ def get_data():
     x, y, z = map(float, input().split())
     return (tx, ty, tz), (qx, qy, qz, qw), (x, y, z)
 
+
 def transform(v, q, p):
-    t = np.array([t[0], t[1], t[2]])
+    t = np.array([v[0], v[1], v[2]])
     p_map = np.array([p[0], p[1], p[2]])
-    rotation = R.from_quat([q[0], q[1], q[2], q[3]])
-    point = rotation.inv().apply(p_map - t)
-    print(round(point[0]), round(point[1]), round(point[2]))
+    qx = q[0]
+    qy = q[1]
+    qz = q[2]
+    qw = q[3]
+    R = np.array([
+    [1 - 2*(qy**2 + qz**2), 2*(qx*qy - qz*qw), 2*(qx*qz + qy*qw)],
+    [2*(qx*qy + qz*qw), 1 - 2*(qx**2 + qz**2), 2*(qy*qz - qx*qw)],
+    [2*(qx*qz - qy*qw), 2*(qy*qz + qx*qw), 1 - 2*(qx**2 + qy**2)]])
+    p_aruco = R.T @ (p_map - t)
+    print(round(p_aruco[0]), round(p_aruco[1]), round(p_aruco[2]))
 
 
 def main():
