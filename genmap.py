8eeb27af6db5cfcb69c3cf2d9f29ff2917bb6c63
Glebas4 <gleb.gurov.jr@gmail.com>
2025-11-15 17:44:30 +0300

    Update genmap.py

--- genmap.py
+++ genmap.py
@@ -5,6 +5,7 @@ import random
 import sys
 import math
 
+
 spawn_service = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
 delete_service = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
 path = "/home/clover/catkin_ws/src/sitl_gazebo/models/pipe"
@@ -14,7 +15,6 @@ class pipe:
     def __init__(self, x, y, path, name):
         self.x = x
         self.y = y
-        self.z = 0
         self.name = name
         self.path = path
         self.pose = Pose()
@@ -30,19 +30,49 @@ class pipe:
                              robot_namespace='',
                              initial_pose=self.pose,
                              reference_frame="world")
-        print(gen.status_message, self.name, "pipe")
+        print(gen.status_message, self.name, f"x={self.x}; y={self.y}")
 
     def delete(self):
         resp = delete_service(self.name)
-        print(resp.status_message, self.name, self.color)
+        print(resp.status_message, self.name)
+
+
+def gen_points():
+    global points
+    points = []
+    while len(points) < 5:
+        x = random.choice([1, -1])
+        y = round(random.uniform(1.05, 8.95), 1)
+        if all(abs(point[1] - y) >=0.75 for point in points): 
+            points.append((x, y))
+
+    return points
 
 
 def main():
+    points = gen_points()
     pipe_main = pipe(1, 1, path+"_main/pipe_main.sdf", "pipe_main")
+    ps1 = pipe(points[0][0], points[0][1], path+"_small/pipe_small.sdf", "pipe_small_1")
+    ps2 = pipe(points[1][0], points[1][1], path+"_small/pipe_small.sdf", "pipe_small_2")
+    ps3 = pipe(points[2][0], points[2][1], path+"_small/pipe_small.sdf", "pipe_small_3")
+    ps4 = pipe(points[3][0], points[3][1], path+"_small/pipe_small.sdf", "pipe_small_4")
+    ps5 = pipe(points[4][0], points[4][1], path+"_small/pipe_small.sdf", "pipe_small_5")
+
+
     if len(sys.argv)>1:
         pipe_main.delete()
+        ps1.delete()
+        ps2.delete()
+        ps3.delete()
+        ps4.delete()
+        ps5.delete()
     else:
         pipe_main.spawn()
+        ps1.spawn()
+        ps2.spawn()
+        ps3.spawn()
+        ps4.spawn()
+        ps5.spawn()
 
 
 
