7e020ac57bcaad8ae02ed707eacdc144fca5a769
Glebas4 <gleb.gurov.jr@gmail.com>
2025-11-16 18:41:45 +0300

    Update genmap.py

--- genmap.py
+++ genmap.py
@@ -19,15 +19,16 @@ class pipe:
     def __init__(self, x, y, path, name, angle=0):
         self.x = x
         self.y = y
+
         self.name = name
         self.path = path
-        self.angle = angle
+
         self.pose = Pose()
         self.pose.position = Point(self.x, self.y, 0)
 
-        if self.angle:
-            self.q = tft.quaternion_from_euler(0, 0, self.angle)
-            self.pose.orientation = Quaternion(*self.q)
+        self.angle = angle
+        self.q = tft.quaternion_from_euler(0, 0, self.angle)
+        self.pose.orientation = Quaternion(*self.q)
     
     def spawn(self):
         with open(self.path, 'r') as f:
@@ -57,32 +58,46 @@ def gen_points():
     return points
 
 
+def gen_pipes(l): #l - длина трубы
+    #Чтобы угол меж основной трубой и врезкой был <= 30,то диапазон угла между ними будет равен разнице смещения и 30 градусов
+    main_angle = random.randint(0, 20)
+    rot_angle = random.randint(main_angle-30, main_angle+30) 
+
+    main_angle = math.radians(main_angle)
+    x = l * math.sin(main_angle) #Смещение второй точки по X и Y 
+    y = l * math.cos(main_angle)
+    
+    return (1, 1, main_angle), (x, y, rot_angle)
+
+
 def main():
     points = gen_points()
-    main_angle = random.randint(0, 30)
-    rot_angle = random.randint(-30, 30)
+    pmain_cords, prot_cords = gen_pipes(l=3)
 
-    pipe_main = pipe(1, 1, path+"_main/pipe_main.sdf", "pipe_main", main_angle)
-    pipe_rot = pipe(3, 3, path+"_main/pipe_main.sdf", "pipe_rot", rot_angle)
+    #pipe_main = pipe(pmain_cords[0], pmain_cords[1], path+"_main/pipe_main.sdf", "pipe_main", pmain_cords[3])
+    #pipe_rot = pipe(prot_cords[0], prot_cords[1], path+"_main/pipe_main.sdf", "pipe_rot", pmain_cords[3])
 
-    #ps1 = pipe(points[0][0], points[0][1], path+"_small/pipe_small.sdf", "pipe_small_1")
-    #ps2 = pipe(points[1][0], points[1][1], path+"_small/pipe_small.sdf", "pipe_small_2")
+    #ps1 - pipe_small №1
+    ps1 = pipe(pmain_cords[0], pmain_cords[1], path+"_small/pipe_small.sdf", "pipe_main", pmain_cords[3])
+    ps2 = pipe(prot_cords[0], prot_cords[1], path+"_small/pipe_small.sdf", "pipe_rot", pmain_cords[3])
     #ps3 = pipe(points[2][0], points[2][1], path+"_small/pipe_small.sdf", "pipe_small_3")
     #ps4 = pipe(points[3][0], points[3][1], path+"_small/pipe_small.sdf", "pipe_small_4")
     #ps5 = pipe(points[4][0], points[4][1], path+"_small/pipe_small.sdf", "pipe_small_5")
 
 
     if len(sys.argv)>1:
-        pipe_main.delete()
-        pipe_rot.delete()
-        #ps1.delete()
-        #ps2.delete()
+        #pipe_main.delete()
+        #pipe_rot.delete()
+        ps1.delete()
+        ps2.delete()
         #ps3.delete()
         #ps4.delete()
         #ps5.delete()
     else:
-        pipe_main.spawn()
-        pipe_rot.spawn()
+        ps1.spawn()
+        ps2.spawn()
+        #pipe_main.spawn()
+        #pipe_rot.spawn()
 
 
 if __name__ == '__main__':
