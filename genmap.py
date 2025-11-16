242823e3fa973c11c568fe0e1e86c55411f6804f
Glebas4 <gleb.gurov.jr@gmail.com>
2025-11-16 19:01:36 +0300

    Update genmap.py

--- genmap.py
+++ genmap.py
@@ -60,12 +60,13 @@ def gen_points():
 
 def gen_pipes(l): #l - длина трубы
     #Чтобы угол меж основной трубой и врезкой был <= 30,то диапазон угла между ними будет равен разнице смещения и 30 градусов
-    main_angle = random.randint(0, 20)
+    #main_angle = random.randint(0, 20)
+    main_angle = 30
     rot_angle = random.randint(main_angle-30, main_angle+30) 
 
     rad = math.radians(main_angle)
-    x = l * math.sin(rad) + 1  #Смещение второй точки по X и Y 
-    y = l * math.cos(rad) + 1
+    x = round(l * math.sin(rad), 2) + 1  #Смещение второй точки по X и Y 
+    y = round(l * math.cos(rad), 2) + 1
     
     return (1, 1, main_angle), (x, y, rot_angle)
 
