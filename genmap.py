fef7d47eb2bba5860b85fd56fa7b0f84fdec0dc0
Glebas4 <gleb.gurov.jr@gmail.com>
2025-11-16 20:03:00 +0300

    Update genmap.py

--- genmap.py
+++ genmap.py
@@ -16,7 +16,7 @@ path = "/home/clover/catkin_ws/src/sitl_gazebo/models/pipe"
 
 
 class pipe:
-    def __init__(self, x, y, path, name, angle=0):
+    def __init__(self, x, y, path, name, angle):
         self.x = x
         self.y = y
 
@@ -46,13 +46,16 @@ class pipe:
         print(resp.status_message, self.name)
 
 
-def gen_points():
-    global points
+def gen_points(x1, x2, y1, y2, n):
     points = []
-    while len(points) < 5:
-        x = random.choice([1, 3])
-        y = round(random.uniform(1.05, 8.95), 2)
-        if all(abs(point[1] - y) >= 0.75 for point in points): 
+    while len(points) != n:
+        x = round(random.uniform(x1, x2), 2)
+        y = round(random.uniform(y1, y2), 2)
+        side = random.choice([True, False])
+        if side:
+            x+=0.71
+            y+=0.71
+        if all(math.sqrt((point[0] - x)**2 + (point[1] - y)**2) >=1 for point in points): #Если гипотенуза соединяющая точки >=1 то координата довабляется
             points.append((x, y))
 
     return points
@@ -61,19 +64,24 @@ def gen_points():
 def gen_pipes(l): #l - длина трубы
     #Чтобы угол меж основной трубой и врезкой был <= 30,то диапазон угла между ними будет равен разнице смещения и 30 градусов
     #main_angle = random.randint(0, 20)
-    main_angle = 30 # 30 degrees = 0.3 Yaw
+    main_angle = 30 # 30 degrees = 0.6 Yaw Gazebo
     rot_angle = random.randint(main_angle-30, main_angle+30) 
 
     rad = math.radians(main_angle)
-    x = round(l * math.sin(rad), 2) + 1  #Смещение второй точки по X и Y 
-    y = round(l * math.cos(rad), 2) + 1
-    
-    return (1, 1, -main_angle/100), (x, y, -rot_angle/100)
+    x = round(l * math.sin(rad), 4) + 1  #Смещение второй точки по X и Y 
+    y = round(l * math.cos(rad), 4) + 0.95 #По идее 1,но лучше 0.95
+
+    loc_angle_rot_pipe = math.radians(rot_angle-main_angle)
+    xk = round(l * math.cos(rad), 4) + 0.95
+    yk = round(l * math.sin(rad), 4) + 0.95 # 0.95 тк радиус трубы 5 см и нужно чтобы она не выпирала
+    #Начало и угол осн трубы; Начало 2ой трубы(конец 1ой) и угол; конец 2 трубы
+    return (1, 1, -main_angle/50), (x, y, -rot_angle/50), (xk, yk)
 
 
 def main():
-    points = gen_points()
-    pmain_cords, prot_cords = gen_pipes(l=1)
+    pmain_cords, prot_cords, prot_end = gen_pipes(l=1)
+    pnt_pipe_main = gen_points(x1=1, x2=prot_cords[0], y1=1, y2=prot_cords[1], n=3)
+    pnt_pipe_rot = gen_points(x1=prot_cords[0], x2=prot_end[0], y1=prot_cords[1], y2=prot_end[1], n=2)
 
     #pipe_main = pipe(pmain_cords[0], pmain_cords[1], path+"_main/pipe_main.sdf", "pipe_main", pmain_cords[3])
     #pipe_rot = pipe(prot_cords[0], prot_cords[1], path+"_main/pipe_main.sdf", "pipe_rot", pmain_cords[3])
@@ -81,9 +89,10 @@ def main():
     #ps1 - pipe_small №1
     ps1 = pipe(pmain_cords[0], pmain_cords[1], path+"_small/pipe_small.sdf", "pipe_main", pmain_cords[2])
     ps2 = pipe(prot_cords[0], prot_cords[1], path+"_small/pipe_small.sdf", "pipe_rot", prot_cords[2])
-    #ps3 = pipe(points[2][0], points[2][1], path+"_small/pipe_small.sdf", "pipe_small_3")
-    #ps4 = pipe(points[3][0], points[3][1], path+"_small/pipe_small.sdf", "pipe_small_4")
-    #ps5 = pipe(points[4][0], points[4][1], path+"_small/pipe_small.sdf", "pipe_small_5")
+    ps3 = pipe(pnt_pipe_main[0][0], pnt_pipe_main[0][1], path+"_small/pipe_small.sdf", "pipe_small_3", pmain_cords[2])
+    ps4 = pipe(pnt_pipe_main[1][0], pnt_pipe_main[1][1], path+"_small/pipe_small.sdf", "pipe_small_4", pmain_cords[2])
+    ps5 = pipe(pnt_pipe_rot[0][0], pnt_pipe_rot[0][1], path+"_small/pipe_small.sdf", "pipe_small_5", prot_cords[2])
+    ps6 = pipe(pnt_pipe_rot[1][0], pnt_pipe_rot[1][1], path+"_small/pipe_small.sdf", "pipe_small_2", prot_cords[2])
 
 
     if len(sys.argv)>1:
@@ -91,12 +100,17 @@ def main():
         #pipe_rot.delete()
         ps1.delete()
         ps2.delete()
-        #ps3.delete()
-        #ps4.delete()
-        #ps5.delete()
+        ps3.delete()
+        ps4.delete()
+        ps5.delete()
+        ps6.delete()
     else:
         ps1.spawn()
         ps2.spawn()
+        ps3.spawn()
+        ps4.spawn()
+        ps5.spawn()
+        ps6.spawn()
         #pipe_main.spawn()
         #pipe_rot.spawn()
 
