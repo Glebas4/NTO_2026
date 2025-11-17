fddaac3f3e4f805ae231f60c898837147fbdc576
Glebas4 <gleb.gurov.jr@gmail.com>
2025-11-17 18:10:05 +0300

    Update genmap.py

--- genmap.py
+++ genmap.py
@@ -52,9 +52,9 @@ def gen_points(x1, x2, y1, y2, n):
         x = round(random.uniform(x1, x2), 2)
         y = round(random.uniform(y1, y2), 2)
         side = random.choice([True, False])
-        if side:
-            x+=0.71
-            y+=0.71
+        #if side:
+         #   x+=0.71
+          #  y+=0.71
         if all(math.sqrt((point[0] - x)**2 + (point[1] - y)**2) >=0.75 for point in points): #Если гипотенуза соединяющая точки >=1 то координата довабляется
             points.append((x, y))
 
@@ -80,42 +80,43 @@ def gen_pipes(l): #l - длина трубы
 
 def main():
     if len(sys.argv)>1:
-        ps1 = pipe(name="pipe_main")
-        ps2 = pipe(name="pipe_rot")
-        ps3 = pipe(name="pipe_small_1")
-        ps4 = pipe(name="pipe_small_2")
-        ps5 = pipe(name="pipe_small_3")
-        ps6 = pipe(name="pipe_small_4")
-        #pipe_main.delete()
-        #pipe_rot.delete()
-        ps1.delete()
-        ps2.delete()
-        ps3.delete()
-        ps4.delete()
-        ps5.delete()
-        ps6.delete()
+        pmain =  pipe(name="pipe_main")
+        prot  =  pipe(name="pipe_rot")
+        #ps1   =  pipe(name="pipe_small_1")
+        #ps2   =  pipe(name="pipe_small_2")
+        #ps3   =  pipe(name="pipe_small_3")
+        #ps4   =  pipe(name="pipe_small_4")
+        #ps5   =  pipe(name="pipe_small_5")
+    
+        pmain.delete()
+        prot.delete()
+        #ps1.delete()
+        #ps2.delete()
+        #ps3.delete()
+        #ps4.delete()
+        #ps5.delete()
     else:
-        pmain_cords, prot_cords, prot_end = gen_pipes(l=1)
-        pnt_pipe_main = gen_points(x1=1, x2=prot_cords[0], y1=1, y2=prot_cords[1], n=3)
-        pnt_pipe_rot = gen_points(x1=prot_cords[0], x2=prot_end[0], y1=prot_cords[1], y2=prot_end[1], n=2)
+        pmain_cords, prot_cords, prot_end = gen_pipes(l=3)
+        #pnt_pipe_main = gen_points(x1=1, x2=prot_cords[0], y1=1, y2=prot_cords[1], n=3)
+        #pnt_pipe_rot = gen_points(x1=prot_cords[0], x2=prot_end[0], y1=prot_cords[1], y2=prot_end[1], n=2)
 
     #pipe_main = pipe(pmain_cords[0], pmain_cords[1], path+"_main/pipe_main.sdf", "pipe_main", pmain_cords[3])
     #pipe_rot = pipe(prot_cords[0], prot_cords[1], path+"_main/pipe_main.sdf", "pipe_rot", pmain_cords[3])
 
     #ps1 - pipe_small №1
-        ps1 = pipe(pmain_cords[0], pmain_cords[1], path+"_small/pipe_small.sdf", "pipe_main", pmain_cords[2])
-        ps2 = pipe(prot_cords[0], prot_cords[1], path+"_small/pipe_small.sdf", "pipe_rot", prot_cords[2])
-        ps3 = pipe(pnt_pipe_main[0][0], pnt_pipe_main[0][1], path+"_small/pipe_small.sdf", "pipe_small_1", pmain_cords[2]-1.8)
-        ps4 = pipe(pnt_pipe_main[1][0], pnt_pipe_main[1][1], path+"_small/pipe_small.sdf", "pipe_small_2", pmain_cords[2]-1.8)
-        ps5 = pipe(pnt_pipe_rot[0][0], pnt_pipe_rot[0][1], path+"_small/pipe_small.sdf", "pipe_small_3", prot_cords[2]-1.8)
-        ps6 = pipe(pnt_pipe_rot[1][0], pnt_pipe_rot[1][1], path+"_small/pipe_small.sdf", "pipe_small_4", prot_cords[2]-1.8)
+        pmain = pipe(pmain_cords[0], pmain_cords[1], path+"_main/pipe_main.sdf", "pipe_main", pmain_cords[2])
+        prot = pipe(prot_cords[0], prot_cords[1], path+"_main/pipe_main.sdf", "pipe_rot", prot_cords[2])
+        #ps3 = pipe(pnt_pipe_main[0][0], pnt_pipe_main[0][1], path+"_small/pipe_small.sdf", "pipe_small_1", pmain_cords[2]-1.8)
+        #ps4 = pipe(pnt_pipe_main[1][0], pnt_pipe_main[1][1], path+"_small/pipe_small.sdf", "pipe_small_2", pmain_cords[2]-1.8)
+        #ps5 = pipe(pnt_pipe_rot[0][0], pnt_pipe_rot[0][1], path+"_small/pipe_small.sdf", "pipe_small_3", prot_cords[2]-1.8)
+        #ps6 = pipe(pnt_pipe_rot[1][0], pnt_pipe_rot[1][1], path+"_small/pipe_small.sdf", "pipe_small_4", prot_cords[2]-1.8)
 
-        ps1.spawn()
-        ps2.spawn()
-        ps3.spawn()
-        ps4.spawn()
-        ps5.spawn()
-        ps6.spawn()
+        pmain.spawn()
+        prot.spawn()
+        #ps3.spawn()
+        #ps4.spawn()
+        #ps5.spawn()
+        #ps6.spawn()
         #pipe_main.spawn()
         #pipe_rot.spawn()
 
