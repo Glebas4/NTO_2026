183e2252ef3d0a659c059e49609dae31ccb52fa4
Glebas4 <gleb.gurov.jr@gmail.com>
2025-11-18 16:43:45 +0300

    Update genmap.py

--- genmap.py
+++ genmap.py
@@ -13,6 +13,7 @@ rospy.init_node("genmap")
 spawn_service = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
 delete_service = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
 path = "/home/clover/catkin_ws/src/sitl_gazebo/models/pipe"
+points = []
 
 
 class pipe:
@@ -48,20 +49,21 @@ class pipe:
 
 def gen_points(x1, x2, y1, y2, n, angle):
     global points
+    pnts = points
     angle = math.radians(angle)
     yc = math.cos(angle)
     xc = math.sin(angle)
-    while len(points) != n:
+    while len(pnts) != n:
         x = round(random.uniform(x1, x2), 2)
         y = round(random.uniform(y1, y2), 2)
         side = random.choice([True, False])
         if side:
             x-=xc
             y-=yc
-        if all(math.sqrt((points[0] - x)**2 + (points[1] - y)**2) >=0.75 for point in points): #Если гипотенуза соединяющая точки >=1 то координата довабляется
-            points.append((x, y))
+        if all(math.sqrt((pnt[0] - x)**2 + (pnt[1] - y)**2) >=0.75 for pnt in pnts): #Если гипотенуза соединяющая точки >=1 то координата довабляется
+            pnts.append((x, y))
 
-    return points
+    return pnts
 
 
 def gen_pipes(l): #l - длина трубы
@@ -71,8 +73,8 @@ def gen_pipes(l): #l - длина трубы
     rot_angle = random.randint(main_angle-30, main_angle+30) 
 
     rad = math.radians(main_angle)
-    x = round(l * math.sin(rad), 4)  #Смещение второй точки по X и Y 
-    y = round(l * math.cos(rad), 4)  #По идее 1,но лучше 0.95
+    x = l * math.sin(rad) #Смещение второй точки по X и Y 
+    y = l * math.cos(rad)  #По идее 1,но лучше 0.95
 
     #0.95 тк радиус трубы 5 см и нужно чтобы она не выпирала
     #Начало и угол осн трубы; Начало 2ой трубы(конец 1ой) и угол; конец 2 трубы
@@ -97,28 +99,27 @@ def main():
         #ps4.delete()
         #ps5.delete()
     else:
-        points = []
         pmain_cords, prot_cords, prot_end, angls = gen_pipes(l=3)
-        pnt_pipe_main = gen_points(x1=1, x2=prot_cords[0], y1=1, y2=prot_cords[1], n=3, angle=angls[0])
-        pnt_pipe_rot = gen_points(x1=prot_cords[0], x2=prot_end[0], y1=prot_cords[1], y2=prot_end[1], n=2, angle=angls[1])
+        main_pnts = gen_points(x1=1, x2=prot_cords[0], y1=1, y2=prot_cords[1], n=3, angle=angls[0])
+        rot_pnts = gen_points(x1=prot_cords[0], x2=prot_end[0], y1=prot_cords[1], y2=prot_end[1], n=2, angle=angls[1])
 
     #pipe_main = pipe(pmain_cords[0], pmain_cords[1], path+"_main/pipe_main.sdf", "pipe_main", pmain_cords[3])
     #pipe_rot = pipe(prot_cords[0], prot_cords[1], path+"_main/pipe_main.sdf", "pipe_rot", pmain_cords[3])
 
     #ps1 - pipe_small №1
-        pmain = pipe(pmain_cords[0], pmain_cords[1], path+"_main/pipe_main.sdf", "pipe_main", pmain_cords[2])
-        prot = pipe(prot_cords[0], prot_cords[1], path+"_main/pipe_main.sdf", "pipe_rot", prot_cords[2])
-        ps1 = pipe(pnt_pipe_main[0][0], pnt_pipe_main[0][1], path+"_small/pipe_small.sdf", "pipe_small_1", pmain_cords[2]-1.8)
-        ps2 = pipe(pnt_pipe_main[1][0], pnt_pipe_main[1][1], path+"_small/pipe_small.sdf", "pipe_small_2", pmain_cords[2]-1.8)
-        ps3 = pipe(pnt_pipe_rot[0][0],  pnt_pipe_rot[0][1],  path+"_small/pipe_small.sdf", "pipe_small_3", prot_cords[2]-1.8)
-        ps4 = pipe(pnt_pipe_rot[1][0],  pnt_pipe_rot[1][1],  path+"_small/pipe_small.sdf", "pipe_small_4", prot_cords[2]-1.8)
+        #pmain = pipe(pmain_cords[0], pmain_cords[1], path+"_main/pipe_main.sdf", "pipe_main", pmain_cords[2])
+        #prot = pipe(prot_cords[0], prot_cords[1], path+"_main/pipe_main.sdf", "pipe_rot", prot_cords[2])
+        #ps1 = pipe(pnt_pipe_main[0][0], pnt_pipe_main[0][1], path+"_small/pipe_small.sdf", "pipe_small_1", pmain_cords[2]-1.8)
+        #ps2 = pipe(pnt_pipe_main[1][0], pnt_pipe_main[1][1], path+"_small/pipe_small.sdf", "pipe_small_2", pmain_cords[2]-1.8)
+        #ps3 = pipe(pnt_pipe_rot[0][0],  pnt_pipe_rot[0][1],  path+"_small/pipe_small.sdf", "pipe_small_3", prot_cords[2]-1.8)
+        #ps4 = pipe(pnt_pipe_rot[1][0],  pnt_pipe_rot[1][1],  path+"_small/pipe_small.sdf", "pipe_small_4", prot_cords[2]-1.8)
 
         pmain.spawn()
         prot.spawn()
-        ps1.spawn()
-        ps2.spawn()
-        ps3.spawn()
-        ps4.spawn()
+        #ps1.spawn()
+        #ps2.spawn()
+        #ps3.spawn()
+        #ps4.spawn()
         #pipe_main.spawn()
         #pipe_rot.spawn()
 
