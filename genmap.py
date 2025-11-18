8f8a32218c347ffd3934c74a0917b3679f7834c9
Glebas4 <gleb.gurov.jr@gmail.com>
2025-11-18 17:27:30 +0300

    Update genmap.py

--- genmap.py
+++ genmap.py
@@ -82,6 +82,7 @@ def gen_pipes(l): #l - длина трубы
 
 
 def main():
+    rospy.wait_for_service('/gazebo/spawn_sdf_model', timeout=10)
     if len(sys.argv)>1:
         pmain =  pipe(name="pipe_main")
         prot  =  pipe(name="pipe_rot")
@@ -100,22 +101,24 @@ def main():
         #ps5.delete()
     else:
         pmain_cords, prot_cords, prot_end, angls = gen_pipes(l=3)
-        main_pnts = gen_points(x1=1, x2=prot_cords[0], y1=1, y2=prot_cords[1], n=3, angle=angls[0])
-        rot_pnts = gen_points(x1=prot_cords[0], x2=prot_end[0], y1=prot_cords[1], y2=prot_end[1], n=2, angle=angls[1])
+        #main_pnts = gen_points(x1=1, x2=prot_cords[0], y1=1, y2=prot_cords[1], n=3, angle=angls[0])
+        #rot_pnts = gen_points(x1=prot_cords[0], x2=prot_end[0], y1=prot_cords[1], y2=prot_end[1], n=2, angle=angls[1])
 
     #pipe_main = pipe(pmain_cords[0], pmain_cords[1], path+"_main/pipe_main.sdf", "pipe_main", pmain_cords[3])
     #pipe_rot = pipe(prot_cords[0], prot_cords[1], path+"_main/pipe_main.sdf", "pipe_rot", pmain_cords[3])
 
     #ps1 - pipe_small №1
-        #pmain = pipe(pmain_cords[0], pmain_cords[1], path+"_main/pipe_main.sdf", "pipe_main", pmain_cords[2])
-        #prot = pipe(prot_cords[0], prot_cords[1], path+"_main/pipe_main.sdf", "pipe_rot", prot_cords[2])
+        pmain = pipe(pmain_cords[0], pmain_cords[1], path+"_main/pipe_main.sdf", "pipe_main", pmain_cords[2])
+        prot = pipe(prot_cords[0], prot_cords[1], path+"_main/pipe_main.sdf", "pipe_rot", prot_cords[2])
         #ps1 = pipe(pnt_pipe_main[0][0], pnt_pipe_main[0][1], path+"_small/pipe_small.sdf", "pipe_small_1", pmain_cords[2]-1.8)
         #ps2 = pipe(pnt_pipe_main[1][0], pnt_pipe_main[1][1], path+"_small/pipe_small.sdf", "pipe_small_2", pmain_cords[2]-1.8)
         #ps3 = pipe(pnt_pipe_rot[0][0],  pnt_pipe_rot[0][1],  path+"_small/pipe_small.sdf", "pipe_small_3", prot_cords[2]-1.8)
         #ps4 = pipe(pnt_pipe_rot[1][0],  pnt_pipe_rot[1][1],  path+"_small/pipe_small.sdf", "pipe_small_4", prot_cords[2]-1.8)
 
         pmain.spawn()
+        rospy.sleep(5)
         prot.spawn()
+        rospy.sleep(5)
         #ps1.spawn()
         #ps2.spawn()
         #ps3.spawn()
