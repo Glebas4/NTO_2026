54f06b17f30d3cf2cd26ed9e992236279e85e9d4
Glebas4 <gleb.gurov.jr@gmail.com>
2025-11-23 18:24:00 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,4 +21,16 @@ cd /home/clover/NTO_2026
 rm -rf models
 rm -rf launch
 
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+sudo apt update
+sudo apt install ros-noetic-image-geometry
+if [ $? -eq 0 ]; then
+    echo "ros-noetic-image-geometry has been downloaded"
+else
+    echo "ERROR: $?"
+fi
+
+
 echo "Setup was completed"
