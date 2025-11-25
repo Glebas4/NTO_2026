95957e4cc36081166775fcb65f2bf53bbe5569cb
Glebas4 <gleb.gurov.jr@gmail.com>
2025-11-25 19:52:07 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ else
     echo "ERROR: $?"
 fi
 
+
 cd /home/clover/NTO_2026/launch
 mv aruco.launch clover.launch /home/clover/catkin_ws/src/clover/clover/launch
 if [ $? -eq 0 ]; then
@@ -17,9 +18,20 @@ else
     echo "ERROR: $?"
 fi
 
+
+cd /home/clover/NTO_2026/front-end
+mv clover.html /home/clover/catkin_ws/src/clover/clover/www
+mv app.js /home/clover/catkin_ws/src/clover/clover/www/js
+if [ $? -eq 0 ]; then
+    echo "Front-end files have been successfully moved to /home/clover/catkin_ws/src/clover/clover/www"
+else
+    echo "ERROR: $?"
+fi
+
 cd /home/clover/NTO_2026
 rm -rf models
 rm -rf launch
+rm -rf front-end
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
@@ -27,7 +39,7 @@ sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31
 sudo apt update
 sudo apt install ros-noetic-image-geometry
 if [ $? -eq 0 ]; then
-    echo "ros-noetic-image-geometry has been downloaded"
+    echo "ros-noetic-image-geometry has been successfully downloaded"
 else
     echo "ERROR: $?"
 fi
