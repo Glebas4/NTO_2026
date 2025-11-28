0b164f17eedd9cbf26cec3c13c025323892dccd4
Glebas4 <gleb.gurov.jr@gmail.com>
2025-11-28 20:15:27 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,6 +21,7 @@ fi
 
 cd /home/clover/NTO_2026/front-end
 mv clover.html /home/clover/catkin_ws/src/clover/clover/www
+mv app.js /home/clover/catkin_ws/src/clover/clover/www/js
 if [ $? -eq 0 ]; then
     echo "Front-end files have been successfully moved to /home/clover/catkin_ws/src/clover/clover/www"
 else
@@ -28,20 +29,35 @@ else
 fi
 
 cd /home/clover/NTO_2026
+mv aruco_map.png /home/clover
+if [ $? -eq 0 ]; then
+    echo "aruco_map.png have been successfully moved to /home/clover"
+else
+    echo "ERROR: $?"
+fi
+
 rm -rf models
 rm -rf launch
 rm -rf front-end
 
+echo "Downloading "image_geometry" package ..."
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 sudo apt update
 sudo apt install ros-noetic-image-geometry
 if [ $? -eq 0 ]; then
-    echo "ros-noetic-image-geometry has been successfully downloaded"
+    echo "ros-noetic-image-geometry has been downloaded"
 else
     echo "ERROR: $?"
 fi
 
 
 echo "Setup was completed"
+echo "____________________USE____________________"
+echo "python3 genmap.py - generate map"
+echo "python3 genmap.py clean - clean world"
+echo "python3 flight.py - start mission"
+echo "http://localhost/clover/clover.html - WEB"
+echo "any questions? - telegram @EPancho8 (main dev)" 
+echo "___________________________________________"
